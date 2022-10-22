e18b844290bbdfcd83d6d4f95ea769dcac5f7c47
aakkaakkii <aakkaakkii1997@gmail.com>
2022-10-22 16:40:26 +0400

    fixes

--- auth-service/src/main/java/com/wine/authservice/repository/UserRepository.java
+++ auth-service/src/main/java/com/wine/authservice/repository/UserRepository.java
@@ -3,14 +3,15 @@ package com.wine.authservice.repository;
 import com.wine.authservice.domain.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByUsername(String username);
-    @Query("SELECT u FROM User u WHERE u.username=:userane AND u.id<>:id")
-    User findByUsernameNotEqualToId(String username, Long id);
+    @Query("SELECT u FROM User u WHERE u.username=:username AND u.id<>:id")
+    User findByUsernameNotEqualToId(@Param("username") String username, @Param("id") Long id);
     User findByEmail(String email);
     @Query("SELECT u FROM User u WHERE u.email=:email AND u.id<>:id")
-    User findByEmailNotEqualToId(String email, Long id);
+    User findByEmailNotEqualToId(@Param("email") String email, @Param("id") Long id);
 }
